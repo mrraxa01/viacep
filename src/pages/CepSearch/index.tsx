a82959c99e1deb88124f46817d73b541bb2b1730
Marcio Rodrigues <mrraxa01@gmail.com>
2023-09-25 21:29:20 -0300

    onSubmit, onChande, form

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -1,19 +1,55 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import ResultCard from '../../components/ResultCard';
 import './styles.css';
 
+type FormData = {
+  cep: string;
+  test: string;
+}
+
 
 const CepSearch = () => {
+
+const [formData, setFormData] = useState<FormData>(
+  {
+    cep: '',
+    test: ''
+  }
+);
+
+const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const name = event.target.name;
+  const value = event.target.value;
+
+  setFormData({ ... formData, [name]:value});
+  
+}
+
+const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+  console.log("clicou =>" + formData.cep +" - " + formData.test)
+}
   return (
     <div className="cep-search-container">
       <h1 className="text-primary">Busca CEP</h1>
       <div className="container search-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-container">
             <input
               type="text"
               className="search-input"
               placeholder="CEP (somente números)"
-              onChange={() => {}}
+              name='cep'
+              value={formData.cep}  
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              className="search-input"
+              placeholder="TESTE INPUT"
+              name='test'
+              value={formData.test}
+              onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
               Buscar
